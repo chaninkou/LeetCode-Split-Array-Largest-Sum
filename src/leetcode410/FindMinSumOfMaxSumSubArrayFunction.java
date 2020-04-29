0cd1ab964da4ca6529fc3c953619789f134b0871
chaninkou <michaelkoulove@yahoo.com>
2020-04-28 20:20:21 -0700

    Redid hard problem on 2D dp unique case.

--- src/leetcode410/FindMinSumOfMaxSumSubArrayFunction.java
+++ src/leetcode410/FindMinSumOfMaxSumSubArrayFunction.java
@@ -7,7 +7,10 @@ public class FindMinSumOfMaxSumSubArrayFunction {
         
         dp[0][0] = nums[0];
         
-        // Get all the sum up til each current element
+        // Each element will be the sum of the left side and current element
+        // Input: [7,2,5,10,8]
+        // [7,2] = 9, [7,2,5] = 14 and so on
+        // [7,9,14,24,32]
         for(int i = 1; i < nums.length; i++){
             dp[0][i] = dp[0][i - 1] + nums[i];
         }
@@ -17,7 +20,19 @@ public class FindMinSumOfMaxSumSubArrayFunction {
            for(int j = i; j < nums.length; j++){
                int min = Integer.MAX_VALUE;
                
-               // Get all the subarray and get the minimum sum for the max sum subarray
+               // j = 1
+               // max of [7] [2] = 7
+               
+               // j = 2
+               // max of [7] [2,5] = 7
+               // max of [7,2] [5] = 9
+               
+               // j = 3
+               // max of [7] [2,5,10] = 17
+               // max of [7,2] [5,10] = 15
+               // max of [7,2,5] [10] = 14
+               
+               // as k increase, that is the number of elements on the left
                for(int k = 0; k < j; k++){
                    min = Math.min(min, Math.max(dp[i - 1][k], dp[0][j] - dp[0][k]));
                }
