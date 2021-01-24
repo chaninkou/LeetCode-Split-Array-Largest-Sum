2245496e05d099d8839af35799cb83ce9eaa66e6
chaninkou <michaelkoulove@yahoo.com>
2021-01-23 19:22:52 -0800

    Redid problem for better understanding.

--- src/leetcode410/FindMinSumOfMaxSumSubArrayFunction.java
+++ src/leetcode410/FindMinSumOfMaxSumSubArrayFunction.java
@@ -10,7 +10,7 @@ public class FindMinSumOfMaxSumSubArrayFunction {
         
         // Each element will be the sum of the left side and current element
         // Input: [7,2,5,10,8]
-        // [7,2] = 9, [7,2,5] = 14 and so on
+        // [7,2] = 9, [7,2,5] = 14 and so on ...
         // [7,9,14,24,32]
         for(int i = 1; i < nums.length; i++){
             dp[0][i] = dp[0][i - 1] + nums[i];
